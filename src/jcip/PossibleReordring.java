1b7da029945bff804b079c88a4e418767480d1cd
lzha99x <27496757@qq.com>
2020-02-20 00:02:15 +0800

    没有同步的情况下，线程是乱序执行。

---
+++ src/jcip/PossibleReordring.java
@@ -0,0 +1,27 @@
+package jcip;
+/**
+ * 如果在程序中没有包含足够的同步，那么可能产生奇怪的结果（不要这么做）。
+ * 可能输出（1，0）或（0，1）
+ * */
+public class PossibleReordring {
+	static int x = 0, y = 0;
+	static int a = 0, b = 0;
+	
+	public static void main(String[] args)
+			throws InterruptedException {
+		Thread one = new Thread(() -> {
+			a = 1;
+			x = b;
+		});
+		
+		Thread two = new Thread(() -> {
+			b = 1;
+			y = a;
+		});
+		
+		one.start(); two.start();
+		one.join(); two.join();
+		System.out.println("("+ x + "," + y + ")");
+	}
+
+}
