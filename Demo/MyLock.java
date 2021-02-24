25024f41534b9b282fa3634fe40763249e290d30
lzha99x <27496757@qq.com>
2021-02-24 16:04:38 +0800

    修改代码支持countDownLatch

--- Demo/MyLock.java
+++ Demo/MyLock.java
@@ -7,24 +7,25 @@ public class MyLock {
         Account target = new Account(1000);
         Thread[] threads = new Thread[TEST_THREAD_COUNT];
 
-        //CountDownLatch countDownLatch = new CountDownLatch(5);
+        CountDownLatch countDownLatch = new CountDownLatch(TEST_THREAD_COUNT);
 
         for (int i = 0; i < TEST_THREAD_COUNT; i++) {
             threads[i] = new Thread(new Runnable(){
                 public void run() {
                     src.transactionToTarget(1, target);
                     //System.out.println(Thread.currentThread());
+                    countDownLatch.countDown();
                 }
             });
             threads[i].start();
         }
 
-        for (int i = 0; i < TEST_THREAD_COUNT; i++) {
-            threads[i].join();
-            //System.out.println("end");
-        }
+        // for (int i = 0; i < TEST_THREAD_COUNT; i++) {
+        //     threads[i].join();
+        //     //System.out.println("end");
+        // }
 
-        //countDownLatch.await();
+        countDownLatch.await();
         System.out.println("src = " + src.getBanalce());
         System.out.println("target = " + target.getBanalce());
     }
